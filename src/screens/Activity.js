3a9f80dabe0b9ff1635e7ed5f144ac6257118e3a
Garrett <gmorganbland@gmail.com>
2018-11-04 20:51:11 -0600

    updated syling for activity

--- src/screens/Activity.js
+++ src/screens/Activity.js
@@ -37,31 +37,42 @@ class Activity extends Component {
   render() {
     return (
         <div>
-          <div>
-            <span className="text-4xl text-grey-darker">Activity</span>
-          </div>
-          <div className="flex flex-row">
             {this.state.entries.map((item) => {
               return (
-                <div key={item.id} className="bg-red">
-                  {item.title}
-                  {item.description}
-                  <button className="text-red" onClick={() => this.removeItem(item.id)}>Remove Item</button>
+                <div key={item.id} className="w-full bg-grey-lighter rounded-lg flex flex-column justify-between items-center mt-4 px-4 py-4">
+                    <div className="flex-row items-center w-2/3 max-w-full">
+                        <div className="text-3xl text-blue-darker">
+                            {item.title}
+                        </div>
+                        <div className="text-grey-dark text-sm pt-2">
+                            {item.description}
+                        </div>
+                    </div>
+                    <div>
+                        <button
+                            onClick={() => this.removeItem(item.id)}
+                            className="bg-grey hover:bg-red-light text-grey-lightest py-2 px-4 rounded-lg inline-flex items-center focus:outline-none"
+                            type="submit">
+                            <svg
+                                className="w-4 h-4"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                >
+                                <polyline points="3 6 5 6 21 6" />
+                                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
               )
             })}
-          </div>
-            <ul>
-              {this.state.entries.map((item) => {
-                return (
-                  <li key={item.id}>
-                    <h3>{item.title}</h3>
-                    <p>{item.description}</p>
-                    <button className="text-red" onClick={() => this.removeItem(item.id)}>Remove Item</button>
-                  </li>
-                )
-              })}
-            </ul>
         </div>
     );
   }
